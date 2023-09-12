44b20c455ea051edb35ec009af1d59e15e406f36
chase.haynes <chase.haynez@gmail.com>
2023-09-12 13:22:45 -0400

    total
    youtube class lesson

---
+++ variables.py
@@ -0,0 +1,26 @@
+# a variable is a set container for storing data values
+
+# 50 mangos
+mangos = 50
+
+# 25 bananas
+bananas = 25
+
+#total sum    50     + 25 = ?
+total_fruit = mangos + bananas
+
+
+name = "I am Chase"
+
+emotion = " and I love my cat"
+
+sentence = name + emotion
+
+# order of variables
+x = 4
+x = "I am chase"
+
+print(type(x))
+
+
+
