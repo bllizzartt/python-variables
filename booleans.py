6263df6030934b890e2e0c8a81a0f09d0a47693d
chase.haynes <chase.haynez@gmail.com>
2023-09-16 18:01:11 -0400

    booleans

---
+++ booleans.py
@@ -0,0 +1,46 @@
+# What is a boolean? A boolean represents on of two values: True and False
+
+# x = 500
+# y = 501
+# a = 50
+# b = 10
+
+# if x > y:
+#     print("This is true! Great job!")
+
+# else:
+#     print("this is wrong! Bad job!")
+
+
+# print(bool("Hello world"))
+# print(bool(9834))
+# print(bool(["Cats","Dogs","dolphins","Chipmunk"]))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool([]))
+# print(bool({}))
+# print(bool(""))
+# print(bool(()))
+
+
+# print(bool(a))
+# print(bool(b))
+
+
+
+
+
+# print(6 < 9)
+# print(1000 > 9024)
+# print(2 == 2)
+
+x = 10 > 4
+def f(x):
+    return x
+if f(x):
+    print("True")
+else:
+    print("False")
+    
