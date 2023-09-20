e667cf91bde1c219474d2abcfb318680f02fbb93
chase.haynes <chase.haynez@gmail.com>
2023-09-20 17:56:59 -0400

    operators

---
+++ operators.py
@@ -0,0 +1,134 @@
+# # operators are used to perform operations on variables and values
+
+# # addition
+# x = 5
+# y = 5
+# z = x + y
+# print(z)
+
+# # subtraction
+# x = 10
+# y = 5
+# z = x - y
+# print(z)
+
+# # multiplication
+# x = 10
+# y = 10
+# z = x * y
+# print(z)
+
+# # division
+# x = 100
+# y = 10
+# z = x / y
+# print(z)
+
+# # modulus v1 = no remainder (0)
+# x = 100
+# y = 10
+# z = x % y
+# print(z)
+
+# # modulus v2 = no remainder (0)
+# x = 103
+# y = 10
+# z = x % y
+# print(z)
+
+# # floor division
+# x = 107.9
+# y = 10.54
+# z = x // y
+# print(z)
+
+# # python assignment operators
+
+# #1
+# #x = 5
+
+# #2
+# # x += 5 
+# # x = x + 5
+
+# #3
+# # x -= 5 
+# # x = x - 5
+
+# #4
+# # x *= 5
+# # x = x * 5
+
+# # 5
+# # x /= 5
+# # x = x /5
+
+# # 6
+# # x %= 5
+# # x = x % 5
+
+# # 7
+# # x **= 5
+# # x = x ** 5
+
+# # 8
+# # x //= 5
+# # x = x // 5
+
+# python comparison operators
+# equals to ==
+"equals"
+x = 3
+y = 5
+"equals to"
+z = x == y
+# print(z)
+
+# not equal 
+x = 5
+y = 5
+z = x != y
+# print(z)
+
+# greater than >
+x = 5 
+y = 10
+z = x > y
+# print(z)
+
+# less than 
+x = 5
+y = 10
+z = x < y
+# print(z)
+
+# less than or equal too
+x = 11
+y = 10
+z = x <= y
+# print(z)
+ 
+# greater than or equal too
+x = 9
+y = 10
+z = x >= y
+# print(z)
+
+
+# python logical operators
+
+# and
+# x = 3
+# y = 5
+# if x > 7 and y < 9:
+#     print("it works")
+# else:
+#     print("boooooo")
+    
+# or
+x = 1
+y = 101
+if x > 2 or y < 100:
+    print("so true")
+else:
+    print("so false")
