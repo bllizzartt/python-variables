c4506512e945cd7b295f3368befe09d77bb10bd6
chase.haynes <chase.haynez@gmail.com>
2023-09-27 19:22:02 -0400

    strings.py

---
+++ lists.py
@@ -0,0 +1,25 @@
+# a list is a single variable that can store multiple values
+
+#1 ordered
+        #0     #1       #2      #3
+cars = ["BMW", "Mazda", "Ford", "McClaren"]
+
+# changeable remove, change, or add
+
+# allows duplicates:
+
+# print(len(cars))
+# int list
+your_age = [43,12,98,24,74]
+# str list
+fav_activity = ["running","biking","hiking","bodybuilding"]
+# bool list
+boolean = [True,False,True,False]
+            #0    #1        #2    #3  #4
+combo_list = [12, "hiking", True, 74, "running"]
+
+# print(your_age)
+# print(fav_activity)
+# print(boolean)
+print(combo_list[2])
+
