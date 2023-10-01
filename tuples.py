dd0f75267cff31420292d7b9472699f75402f351
chase.haynes <chase.haynez@gmail.com>
2023-09-30 21:02:51 -0400

    tuples

---
+++ tuples.py
@@ -0,0 +1,24 @@
+# A tuple is used to store multiple items in a single variable
+
+# ordered
+# index 0 - 10
+
+# unchangeable
+# we cannot add or remove from the tuple once it has be created
+
+# allows duplicates
+# same value for different indexes
+
+# more static 
+# faster to index through it
+
+             #0           #1      #2      #3
+str_tuple = ("dolphins", "cats", "dogs", "spider")
+            #0 #1 #2 #3
+int_tuple = (1, 2, 3, 4)
+              #0    #1     #2   #3
+bool_tuple = (True, False, False, True)
+
+# print(len(str_tuple[2]))
+# print(int_tuple[3])
+print(bool_tuple[2])
