2cc6750f39cd6e6b0d5ad99ee650390ab8ce68c7
chase.haynes <chase.haynez@gmail.com>
2023-10-04 19:52:00 -0400

    sets

---
+++ sets.py
@@ -0,0 +1,27 @@
+#unordered
+
+#unchangebale 
+
+# does not allow duplicates
+# bc it is not indexed
+
+# sets can be int, str, booleans
+        #0      #1     #2  #3  #4
+# myset = {"Cat", "Dog", 10, 8, True}
+
+#set
+my_pets = {"Cat", "Dog", "Bird", "Rat", "Dragon"}
+#set
+# pets_needed = {"Lizard", "Mouse", "Spider"}
+
+#lists
+
+pets_needed = ["Lizard", "Mouse", "Spider"]
+
+my_pets.update(pets_needed)
+
+#add
+# myset.add("Bird")
+
+print(my_pets)
+
